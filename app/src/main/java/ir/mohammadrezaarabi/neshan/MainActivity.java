6b156c8fe29c81756e00bba7a1ffc1bbfffd5d40
mohammadreza6969 <3ecurityy@gmail.com>
2021-11-04 22:19:39 +0330

    create project

---
+++ app/src/main/java/ir/mohammadrezaarabi/neshan/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.mohammadrezaarabi.neshan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
